b8493e9f1c76b686af325ac74b2412bd6e798279
Eric McCorkle <emc2@metricspace.net>
2013-11-24 22:29:09 -0500

    More work on the type checker

--- Language/Salt/Core/TypeCheck.hs
+++ Language/Salt/Core/TypeCheck.hs
@@ -24,6 +24,7 @@ module Language.Salt.Core.TypeCheck(
        ) where
 
 import Control.Monad.TypeCheck.Class
+import Data.Default
 import Data.Pos
 import Language.Salt.Core.Syntax
 
@@ -31,22 +32,79 @@ import qualified Data.Map as Map
 
 type TypeContext sym = Map.Map sym (Term sym sym)
 
-isSubType :: (MonadTypeCheck sym m, Ord sym) =>
-             TypeContext sym
-          -> Term sym sym
-          -> Term sym sym
-          -> m Bool
-isSubType = error "XXX not implemented"
+-- Utility function to get the type of a symbol
+getSymType :: (MonadTypeCheck sym m, Default sym, Ord sym) =>
+              TypeContext sym -> sym -> Pos -> m (Term sym sym)
+getSymType ctx sym p =
+ case Map.lookup sym ctx of
+   -- If the context has a type, return it
+   Just ty -> return ty
+   -- Otherwise log an undefined symbol error
+   Nothing -> undefSym sym p
 
-checkType :: (MonadTypeCheck sym m, Ord sym) =>
+checkSubType :: (MonadTypeCheck sym m, Default sym, Ord sym) =>
+             TypeContext sym
+          -- ^ The type check context
+          -> Term sym sym
+          -- ^ The supposed subtype
+          -> Term sym sym
+          -- ^ The supposed supertype
+          -> m Bool
+-- For function types, the return type is covariant, and the arguments
+-- are contravariant.
+checkSubType ctx ProdType {} ProdType {} = error "XXX not implemented"
+-- For record types, the subtype must have all the fields of the
+-- supertype, and they must be subtypes of the corresponding fields in
+-- the supertype.  However, there can be extra fields in the subtype.
+checkSubType ctx SumType {} SumType {} = error "XXX not implemented"
+-- For refinement types, the base types are covariant, and the
+-- predicate of the subtype must imply the predicate of the supertype.
+checkSubType ctx RefineType { refineType = subbasety }
+                 RefineType { refineType = superbasety } =
+  do
+    out <- checkSubType ctx subbasety superbasety
+    -- Only generate a proof obligation if the subtype check succeeded
+    if out
+      then error "XXX not implemented"
+      else return False
+-- For computation types, the result type is covariant, and the
+-- specification must be a sub-specification of the supertype's
+-- specification
+checkSubType ctx CompType { compType = subresty }
+                 CompType { compType = superresty } =
+  do
+    out <- checkSubType ctx subresty superresty
+    -- Only generate a proof obligation if the subtype check succeeded
+    if out
+      then error "XXX not implemented"
+      else return False
+-- For anything else, log a type error
+checkSubType _ _ _ = error "XXX not implemented"
+
+-- | Check pattern match cases.
+checkCases :: (MonadTypeCheck sym m, Default sym, Ord sym) =>
+              TypeContext sym
+           -- ^ The type context
+           -> Term sym sym
+           -- ^ The term being type checked
+           -> [Case bound free]
+           -- ^ The term being type checked
+           -> m [Case bound free]
+checkCases _ _ _ = error "XXX not implemented"
+
+-- | Check that a type is well-formed.  At present, it is impossible to construct an arbitrary-rank type, so it is sufficient to 
+checkType :: (MonadTypeCheck sym m, Default sym, Ord sym) =>
              TypeContext sym
           -- ^ The type context
           -> Term sym sym
           -- ^ The term being type checked
           -> m (Term sym sym)
-checkType _ _ = error "XXX not implemented"
+checkType ctx term =
+  do
+    typety <- typeType
+    checkIntroTerm ctx typety term
 
-checkIntroTerm :: (MonadTypeCheck sym m, Ord sym) =>
+checkIntroTerm :: (MonadTypeCheck sym m, Default sym, Ord sym) =>
                   TypeContext sym
                -- ^ The type context.
                -> Term sym sym
@@ -77,7 +135,20 @@ checkIntroTerm ctx RefineType { refineType = basety, refineCases = cases,
 -- Also, ignore it if we try to check a bad term
 checkIntroTerm _ _ bad @ (BadTerm _) = return bad
 -- For quantified terms, it's ok to assert that it's a proposition
-checkIntroTerm ctx ty Quantified {} = error "XXX not implemented yet"
+checkIntroTerm ctx ty term @ Quantified { quantType = quantty,
+                                          quantCases = cases,
+                                          quantPos = p } =
+  do
+    propty <- propType
+    -- The expected type must be exactly the prop type
+    if propty == ty
+      -- If it is, then check the type and then the cases
+      then do
+        quantty' <- checkType ctx quantty
+        cases' <- checkCases ctx quantty' cases
+        return term { quantType = quantty', quantCases = cases' }
+      -- Otherwise, log a type error
+      else formMismatch term ty
 -- For lambda terms, it is ok to assert that it's a function type, as
 -- long as the arguments match up.
 checkIntroTerm ctx ProdType {} Lambda {} = error "XXX not implemented yet"
@@ -97,51 +168,54 @@ checkIntroTerm ctx ty term @ Comp {} = formMismatch term ty
 -- subtype of the expected type.
 checkIntroTerm ctx ty term =
   do
-    actualty <- checkElimTerm ctx term
-    subtype <- isSubType ctx actualty ty
+    (term', actualty) <- checkElimTerm ctx term
+    subtype <- checkSubType ctx actualty ty
     if subtype
-      then return term
+      then return term'
       else return (BadTerm (pos term))
 
 -- | Type check elimination terms.  These terms produce a type during
 -- type checking, as opposed to introduction terms, which require a
 -- type to check a term against.
-checkElimTerm :: (MonadTypeCheck sym m, Ord sym) =>
+checkElimTerm :: (MonadTypeCheck sym m, Default sym, Ord sym) =>
                  TypeContext sym
               -- ^ The type context.
               -> Term sym sym
               -- ^ The term being type checked.
-              -> m (Term sym sym)
+              -> m (Term sym sym, Term sym sym)
+              -- ^ The rewritten term and the type synthesized by the term
+
 -- For calls, expect to get a function type.  Check all the arguments,
 -- then figure out what the return type should be.
-checkElimTerm ctx Call { callFunc = func } =
+checkElimTerm ctx Call { callFunc = func, callPos = p } =
   do
-    functy <- checkElimTerm ctx func
+    (func', functy) <- checkElimTerm ctx func
     case functy of
       -- Now, check that the arguments match, and then figure out the
       -- return type.
       ProdType {} -> error "XXX not implemented yet"
       -- Pass bad types on through
-      bad @ (BadTerm _) -> return bad
+      bad @ (BadTerm _) -> return (BadTerm p, bad)
       -- Anything else is an error
-      _ -> expectedFunction func functy
+      _ ->
+        do
+          badty <- expectedFunction func functy
+          return (BadTerm p, badty)
 -- For typed terms, use the stated type to check the introduction term
-checkElimTerm ctx Typed { typedTerm = term, typedType = ty } =
+checkElimTerm ctx typed @ Typed { typedTerm = term, typedType = ty } =
   do
     -- First check the type, in case it's bad, and also to resolve any
     -- type symbols.
     ty' <- checkType ctx ty
-    checkIntroTerm ctx ty' term
-    return ty'
+    term' <- checkIntroTerm ctx ty' term
+    return (typed { typedTerm = term', typedType = ty' }, ty')
 -- For a symbol, look it up in the type context and return it
-checkElimTerm ctx Var { varSym = sym, varPos = p } =
-  case Map.lookup sym ctx of
-    -- If the context has a type, return it
-    Just ty -> return ty
-    -- Otherwise log an undefined symbol error
-    Nothing -> undefSym sym p
+checkElimTerm ctx term @ Var { varSym = sym, varPos = p } =
+  do
+    ty' <- getSymType ctx sym p
+    return (term, ty')
 -- Pass errors on through
-checkElimTerm _ (BadTerm pos) = return (BadTerm pos)
+checkElimTerm _ term @ (BadTerm _) = return (term, BadTerm (pos term))
 -- Attempting to check anything else is a compiler bug
 checkElimTerm _ _ =
   error "Compiler error: attempting elimination term type check on a non-elimination term"
