4cf13085f76cfcfcc57cce9a559aedec8c41871c
Eric McCorkle <emc2@metricspace.net>
2014-12-02 09:38:54 -0500

    Changed name of class datatypes to kind, added TruthKind

--- src/library/Language/Salt/Surface/Common.hs
+++ src/library/Language/Salt/Surface/Common.hs
@@ -1,4 +1,4 @@
--- Copyright (c) 2013 Eric McCorkle.
+-- Copyright (c) 2014 Eric McCorkle.
 --
 -- This program is free software; you can redistribute it and/or
 -- modify it under the terms of the GNU General Public License as
@@ -15,21 +15,25 @@
 -- Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
 -- 02110-1301 USA
 {-# OPTIONS_GHC -funbox-strict-fields -Wall -Werror #-}
+{-# LANGUAGE FlexibleInstances, MultiParamTypeClasses #-}
 
 -- | A module containing common structures for both AST and Syntax.
 module Language.Salt.Surface.Common(
-       ScopeClass(..),
-       TruthClass(..),
+       BuilderKind(..),
+       TruthKind(..),
+       AbstractionKind(..),
        AliasClass(..),
        Visibility(..)
        ) where
 
+import Data.Array
 import Data.Hashable
 import Text.Format
+import Text.XML.Expat.Pickle
 
 -- | Scope classes.  These define the exact semantics of a scoped
 -- entity declaration.
-data ScopeClass =
+data BuilderKind =
     -- | Signatures are similar to SML signatures, or Fortress traits.
     -- A signature declares a type, which may not be directly
     -- instantiated, and whose mutable fields are not automatically
@@ -43,11 +47,11 @@ data ScopeClass =
     -- | Modules are similar to SML modules.  Unlike SML, a module may
     -- be used at runtime.  Modules declare a named instance of an
     -- anonymous record type
-    -- 
+    --
     -- A module declaration is tantamount to declaring a structure
     -- type anonymously, followed by a single instance of the type,
     -- then initializing the instance.
-    -- 
+    --
     -- Put in terms of classes, modules are like declaring a class
     -- anonymously, then instantiating it.
   | Module
@@ -57,11 +61,12 @@ data ScopeClass =
     -- as a supertype.  Multiple inheritence of classes is not
     -- allowed.
   | Class
-    deriving (Ord, Eq)
+  | Typeclass
+    deriving (Ord, Eq, Enum)
 
 -- | Truth classes.  These define the exact semantics of a truth
 -- declaration.
-data TruthClass =
+data TruthKind =
     -- | Theorems are propositions that are added to the proof
     -- environments of every proof obligation generated from within
     -- this scope.  Abstract declarations of theorems do not need to
@@ -72,6 +77,16 @@ data TruthClass =
     -- the pre- and post-conditions of every state transition in the
     -- current scope.  They do not need to be proven.
   | Invariant
+    deriving (Ord, Eq, Enum)
+
+data AbstractionKind =
+    -- | A function abstraction.
+    Lambda
+    -- | A forall proposition.
+  | Forall
+    -- | An exists proposition.
+  | Exists
+    deriving (Ord, Eq, Enum)
 
 -- | Alias classes.  These define the exact semantics of an alias.
 data AliasClass =
@@ -85,7 +100,7 @@ data AliasClass =
     -- | Exports alter the visibility and possibly the name of a
     -- declaration.  By default, they make a given declaration public.
   | Export
-    deriving (Ord, Eq)
+    deriving (Ord, Eq, Enum)
 
 -- | Visibilities.  Controls when accesses to a given element are legal.
 data Visibility =
@@ -98,43 +113,83 @@ data Visibility =
   | Protected
     -- | Public access.  The given element may be accessed anywhere.
   | Public
-    deriving (Ord, Eq)
+    deriving (Ord, Eq, Enum, Ix)
 
-instance Hashable ScopeClass where
-  hashWithSalt s Signature = s `hashWithSalt` (1 :: Int)
-  hashWithSalt s Interface = s `hashWithSalt` (2 :: Int)
-  hashWithSalt s Module = s `hashWithSalt` (3 :: Int)
-  hashWithSalt s Class = s `hashWithSalt` (4 :: Int)
+instance Hashable BuilderKind where
+  hashWithSalt s = hashWithSalt s . fromEnum
 
-instance Hashable TruthClass where
-  hashWithSalt s Theorem = s `hashWithSalt` (1 :: Int)
-  hashWithSalt s Invariant = s `hashWithSalt` (2 :: Int)
+instance Hashable AbstractionKind where
+  hashWithSalt s = hashWithSalt s . fromEnum
 
-instance Hashable AliasClass where
-  hashWithSalt s Import = s `hashWithSalt` (1 :: Int)
-  hashWithSalt s Open = s `hashWithSalt` (2 :: Int)
-  hashWithSalt s Export = s `hashWithSalt` (3 :: Int)
+instance (GenericXMLString tag, Show tag, GenericXMLString text, Show text) =>
+         XmlPickler (Attributes tag text) BuilderKind where
+  xpickle = xpAlt fromEnum
+                  [xpWrap (const Signature, const ())
+                          (xpAttrFixed (gxFromString "kind")
+                                       (gxFromString "Signature")),
+                   xpWrap (const Interface, const ())
+                          (xpAttrFixed (gxFromString "kind")
+                                       (gxFromString "Interface")),
+                   xpWrap (const Module, const ())
+                          (xpAttrFixed (gxFromString "kind")
+                                       (gxFromString "Module")),
+                   xpWrap (const Class, const ())
+                          (xpAttrFixed (gxFromString "kind")
+                                       (gxFromString "Class")),
+                   xpWrap (const Typeclass, const ())
+                          (xpAttrFixed (gxFromString "kind")
+                                       (gxFromString "Typeclass"))]
 
-instance Hashable Visibility where
-  hashWithSalt s Private = s `hashWithSalt` (1 :: Int)
-  hashWithSalt s Protected = s `hashWithSalt` (2 :: Int)
-  hashWithSalt s Public = s `hashWithSalt` (3 :: Int)
-
-instance Show ScopeClass where
+instance Show BuilderKind where
   show Signature = "signature"
   show Interface = "interface"
   show Module = "module"
   show Class = "class"
+  show Typeclass = "typeclass"
 
-instance Show TruthClass where
+instance Format BuilderKind where format = string . show
+
+instance Hashable TruthKind where
+  hashWithSalt s = hashWithSalt s . fromEnum
+
+instance (GenericXMLString tag, Show tag, GenericXMLString text, Show text) =>
+         XmlPickler (Attributes tag text) TruthKind where
+  xpickle = xpAlt fromEnum
+                  [xpWrap (const Theorem, const ())
+                          (xpAttrFixed (gxFromString "kind")
+                                       (gxFromString "Theorem")),
+                   xpWrap (const Invariant, const ())
+                          (xpAttrFixed (gxFromString "kind")
+                                       (gxFromString "Invariant"))]
+
+instance Show TruthKind where
   show Theorem = "theorem"
   show Invariant = "invariant"
+
+instance Format TruthKind where format = string . show
+
+instance Hashable AliasClass where
+  hashWithSalt s = hashWithSalt s . fromEnum
+
+instance Hashable Visibility where
+  hashWithSalt s = hashWithSalt s . fromEnum
+
+instance (GenericXMLString tag, Show tag, GenericXMLString text, Show text) =>
+         XmlPickler (Attributes tag text) Visibility where
+  xpickle = xpAlt fromEnum
+                  [xpWrap (const Private, const ())
+                          (xpAttrFixed (gxFromString "visibility")
+                                       (gxFromString "Private")),
+                   xpWrap (const Protected, const ())
+                          (xpAttrFixed (gxFromString "visibility")
+                                       (gxFromString "Protected")),
+                   xpWrap (const Public, const ())
+                          (xpAttrFixed (gxFromString "visibility")
+                                       (gxFromString "Public"))]
 
 instance Show Visibility where
   show Private = "private"
   show Protected = "protected"
   show Public = "public"
 
-instance Format ScopeClass where format = text . show
-instance Format TruthClass where format = text . show
-instance Format Visibility where format = text . show
+instance Format Visibility where format = string . show
