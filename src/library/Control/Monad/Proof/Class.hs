a6cb591ebdb7cac989e76f2606a89d3c1ecc6b17
Eric McCorkle <emc2@metricspace.net>
2015-05-13 22:38:58 -0400

    Changed license to BSD

--- src/library/Control/Monad/Proof/Class.hs
+++ src/library/Control/Monad/Proof/Class.hs
@@ -1,20 +1,32 @@
--- Copyright (c) 2013, 2014 Eric McCorkle.
+-- Copyright (c) 2015 Eric McCorkle.  All rights reserved.
 --
--- This program is free software; you can redistribute it and/or
--- modify it under the terms of the GNU General Public License as
--- published by the Free Software Foundation; either version 2 of the
--- License, or (at your option) any later version.
+-- Redistribution and use in source and binary forms, with or without
+-- modification, are permitted provided that the following conditions
+-- are met:
 --
--- This program is distributed in the hope that it will be useful, but
--- WITHOUT ANY WARRANTY; without even the implied warranty of
--- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
--- General Public License for more details.
+-- 1. Redistributions of source code must retain the above copyright
+--    notice, this list of conditions and the following disclaimer.
 --
--- You should have received a copy of the GNU General Public License
--- along with this program; if not, write to the Free Software
--- Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
--- 02110-1301 USA
-
+-- 2. Redistributions in binary form must reproduce the above copyright
+--    notice, this list of conditions and the following disclaimer in the
+--    documentation and/or other materials provided with the distribution.
+--
+-- 3. Neither the name of the author nor the names of any contributors
+--    may be used to endorse or promote products derived from this software
+--    without specific prior written permission.
+--
+-- THIS SOFTWARE IS PROVIDED BY THE AUTHORS AND CONTRIBUTORS ``AS IS''
+-- AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+-- TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+-- PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHORS
+-- OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+-- SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+-- LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+-- USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+-- ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+-- OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+-- OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+-- SUCH DAMAGE.
 {-# OPTIONS_GHC -funbox-strict-fields -Wall -Werror #-}
 {-# LANGUAGE MultiParamTypeClasses, FlexibleInstances #-}
 
@@ -33,28 +45,28 @@ import Language.Salt.Core.Syntax
 -- | This is a monad class containing rules for the five axioms of the
 -- intuitionistic predicate logic upon which the Salt language is
 -- built.
--- 
+--
 -- This class is only designed with checking or recording proofs in
 -- mind, not synthesizing them (ie in an automated prover).  Hence the
 -- monad produces no usable value, the assumption being that whatever
 -- artifact is produced is contained within the monad.
--- 
+--
 -- One use of this is to simply assert the applicability of these
 -- rules, then update the truth and type contexts and the goals
 -- accordingly, and emit error messages whenever a rule fails to
 -- apply.
--- 
+--
 -- Another possible use is simply to emit proof scripts for Coq or a
 -- similar prover.  The type checker can use this to produce a "flight
 -- record", or to sanity-check itself with the proof checker as it
 -- goes.
--- 
+--
 -- An automated prover could likewise use this as a separate module to
 -- encapsulate its internal state, and die with an error if a rule
 -- fails to apply (presumably, it would have another monad class that
 -- allows it to examine the goal and context state to determine if a
 -- rule is applicable).
--- 
+--
 -- The names of these axioms are borrowed in part
 -- from Coq tactics.
 class Monad m => MonadProof sym m where
